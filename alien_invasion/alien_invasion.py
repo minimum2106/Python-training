6ad7ca5e5c5ae902962f2b68e1cfe883e700cda4
unknown <tranhongquan2106@gmail.com>
2021-04-04 09:50:14 +0700

    added 2 modules: game_functions and settings

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,6 +1,11 @@
 import sys
 import pygame
+
+
 from settings import Settings 
+from ship import Ship
+
+import game_functions as gf 
 
 
 def run_game():
@@ -11,12 +16,12 @@ def run_game():
     )
     pygame.display.set_caption("Alien Invasion")
 
+    ship = Ship(ai_settings, screen)
+
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+        gf.check_events(ship)
+        ship.update()
+        gf.update_screen(ai_settings, screen, ship)
 
-        screen.fill(ai_settings.bg_color)
 
-        pygame.display.flip()
-
+run_game()
